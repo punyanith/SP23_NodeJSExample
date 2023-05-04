d5d4d84734db08802bb64738bc493b6b4c55dc08
Pun <punyanith.t@gmail.com>
2023-05-04 10:46:46 -0600

    add files

---
+++ fruitloops.js
@@ -0,0 +1,6 @@
+
+const fruit = ["Apple", "Cherry", "Banana", "Kiwi"];
+
+fruits.forEach((item, index) => {
+    console.log(item, index);
+});
---
+++ helloworld.js
@@ -0,0 +1,3 @@
+const myName = "Grant";
+
+console.log("Hello " + myName)
